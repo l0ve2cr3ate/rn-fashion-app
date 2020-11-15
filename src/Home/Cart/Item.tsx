49e3ab12d3ae2769e6fd6f28eadab90c0b39eb33
l0ve2cr3ate <l0ve2cr3ate@gmail.com>
2020-11-15 10:09:34 +0100

    refactor: Change styling of Item component.

--- src/Home/Cart/Item.tsx
+++ src/Home/Cart/Item.tsx
@@ -1,24 +1,47 @@
-import React from "react";
+import React, { FC } from "react";
 
 import { Box, Text } from "../../components";
+import SwipeableRow from "./SwipeableRow";
 
-const Item = () => {
+interface ItemProps {
+  onDelete: () => void;
+}
+
+const Item: FC<ItemProps> = ({ onDelete }) => {
   return (
-    <Box padding="m" flexDirection="row">
-      <Box
-        width={120}
-        height={120}
-        borderRadius="m"
-        style={{ backgroundColor: "#BFEAF5" }}
-      />
-      <Box padding="m">
-        <Text variant="header">Size M, L</Text>
-        <Text variant="title3">Short Sleeve Organic Top</Text>
-        <Text variant="title3" color="primary">
-          $29.99
-        </Text>
+    <SwipeableRow onDelete={onDelete}>
+      <Box padding="m" flexDirection="row" alignItems="center">
+        <Box
+          width={120}
+          height={120}
+          borderRadius="m"
+          style={{ backgroundColor: "#BFEAF5" }}
+        />
+        <Box padding="m" flex={1} justifyContent="center">
+          <Text variant="header">Size M, L</Text>
+          <Text variant="title3">Short Sleeve Organic Top</Text>
+          <Text variant="title3" color="primary">
+            $29.99
+          </Text>
+        </Box>
+        <Box justifyContent="center">
+          <Box
+            backgroundColor="secondary"
+            style={{
+              justifyContent: "center",
+              alignItems: "center",
+              width: 24,
+              height: 24,
+              borderRadius: 12,
+            }}
+          >
+            <Text variant="header" color="background">
+              x2
+            </Text>
+          </Box>
+        </Box>
       </Box>
-    </Box>
+    </SwipeableRow>
   );
 };
 
