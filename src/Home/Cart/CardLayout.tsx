a19cbad72369d3cfa8ce70dfb0d1e9303983a97a
l0ve2cr3ate <l0ve2cr3ate@gmail.com>
2020-11-19 22:47:08 +0100

    feat: Create CardLayout component for displaying creditcard info in Cart.

---
+++ src/Home/Cart/CardLayout.tsx
@@ -0,0 +1,48 @@
+import { BoxProps } from "@shopify/restyle";
+import React, { FC, ReactNode } from "react";
+
+import { BorderlessTap, Box } from "../../components";
+import { Theme } from "../../components/Theme";
+
+export const CARD_HEIGHT = 160;
+const CARD_WIDTH = 120;
+
+export enum CardType {
+  VISA,
+  MASTERCARD,
+}
+export interface CardModel {
+  id: number;
+  type: CardType;
+  last4Digits: number;
+  expiration: string;
+}
+
+interface CardLayoutProps {
+  children: ReactNode;
+  onPress: () => void;
+  backgroundColor: BoxProps<Theme>["backgroundColor"];
+}
+
+const CardLayout: FC<CardLayoutProps> = ({
+  onPress,
+  children,
+  backgroundColor,
+}) => {
+  return (
+    <BorderlessTap onPress={onPress}>
+      <Box
+        padding="m"
+        marginLeft="m"
+        borderRadius="m"
+        width={CARD_WIDTH}
+        height={CARD_HEIGHT}
+        backgroundColor={backgroundColor}
+      >
+        {children}
+      </Box>
+    </BorderlessTap>
+  );
+};
+
+export default CardLayout;
